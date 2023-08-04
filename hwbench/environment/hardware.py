1463cfb325b8415299dbb1583beaf9e1a4cb53e8
Nathanaëlle Mouniée <n.mouniee@criteo.com>
2023-08-04 17:04:49 +0200

    hwbench: add helpers to dump and extract files [2/2]
    `use extract_file_from_tar` and `create_tar_from_directory` in
    hardware.py

--- hwbench/environment/hardware.py
+++ hwbench/environment/hardware.py
@@ -1,31 +1,51 @@
 import os
 import pathlib
-import logging
+from typing import Optional
+
+from hwbench.archive.archive import create_tar_from_directory, extract_file_from_tar
 
 
 class Hardware:
-    def __init__(self, out_dir):
+    SYS_DMI = "/sys/devices/virtual/dmi/id/"
+    ARCH_DMI = "dmi-info.tar"
+
+    def __init__(self, out_dir: pathlib.Path):
         self.out_dir = out_dir
 
-    # from conformity
     @staticmethod
-    def get_dmi_from_sysfs(entry, root_path="/sys/devices/virtual/dmi/id/"):
-        """Extract DMI & BMC information from the host."""
-        try:
-            return pathlib.Path(root_path + entry).read_text().replace("\n", "")
-        except Exception as error:
-            logging.error(f"Could not get dmi information {error}")
-            return ""
+    def bytes_to_dmi_info(payload: Optional[bytes]) -> Optional[str]:
+        if payload is None:
+            return None
+        return payload.decode("utf-8", "strict").replace("\n", "")
 
-    def dump(self):
+    @staticmethod
+    def extract_dmi_payload(
+        tarfile: pathlib.Path, file: str, root_path=SYS_DMI
+    ) -> Optional[bytes]:
+        return extract_file_from_tar(tarfile.as_posix(), os.path.join(root_path, file))
+
+    def dump(self) -> dict[str, Optional[str]]:
+        tarfilename = self.out_dir.joinpath(self.ARCH_DMI)
+        create_tar_from_directory(tarfilename.as_posix(), self.SYS_DMI)
+
         return {
             # TODO: more, or even dmidecode parsing
-            "vendor": self.get_dmi_from_sysfs("sys_vendor"),
-            "product": self.get_dmi_from_sysfs("product_name"),
-            "serial": self.get_dmi_from_sysfs("product_serial"),
+            "vendor": self.bytes_to_dmi_info(
+                self.extract_dmi_payload(tarfilename, "sys_vendor")
+            ),
+            "product": self.bytes_to_dmi_info(
+                self.extract_dmi_payload(tarfilename, "product_name")
+            ),
+            "serial": self.bytes_to_dmi_info(
+                self.extract_dmi_payload(tarfilename, "product_serial")
+            ),
             "bios": {
-                "version": self.get_dmi_from_sysfs("bios_version"),
-                "release": self.get_dmi_from_sysfs("bios_release"),
+                "version": self.bytes_to_dmi_info(
+                    self.extract_dmi_payload(tarfilename, "bios_version")
+                ),
+                "release": self.bytes_to_dmi_info(
+                    self.extract_dmi_payload(tarfilename, "bios_release")
+                ),
             },
             "sysconf_threads": os.sysconf("SC_NPROCESSORS_ONLN"),
         }
