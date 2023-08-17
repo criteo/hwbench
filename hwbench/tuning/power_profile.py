5c3d0ee82c8039e361d75282b482b4015014a6ee
Nathanaëlle Mouniée <n.mouniee@criteo.com>
2023-08-17 15:13:24 +0200

    hwbench: skip tuning if condition met

--- hwbench/tuning/power_profile.py
+++ hwbench/tuning/power_profile.py
@@ -6,8 +6,21 @@ import re
 class PerformancePowerProfile:
     def __init__(self, out_dir):
         self.out_dir = out_dir
+        self.skip_tuning = False
 
-    def run(self):
+        available_governors = (
+            pathlib.Path("/sys/devices/system/cpu/cpuidle/available_governors")
+            .read_text("ascii")
+            .strip()
+            .split()
+        )
+        self.skip_tuning |= (
+            len(available_governors) == 1 & available_governors[0] == "menu"
+        )
+
+    def run(self) -> None:
+        if self.skip_tuning:
+            return
         pattern = re.compile("cpu[0-9]+")
         for rootpath, dirnames, filenames in os.walk("/sys/devices/system/cpu"):
             for dirname in dirnames:
