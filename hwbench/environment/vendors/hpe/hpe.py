104ff5596d2dab621006d058b8a5f8f51500a320
Anisse Astier <an.astier@criteo.com>
2023-08-23 19:16:51 +0200

    vendors/hpe: remove unused line
    This is a remnant before the code got its current form.

--- hwbench/environment/vendors/hpe/hpe.py
+++ hwbench/environment/vendors/hpe/hpe.py
@@ -1,11 +1,9 @@
 from ..vendor import Vendor
-from ...dmi import DmiSys
 from .ilorest import Ilorest, IlorestServerclone
 
 
 class Hpe(Vendor):
     def detect(self) -> bool:
-        self.out_dir.joinpath(DmiSys.ARCH_DMI)
         return self.dmi.info("sys_vendor") == "HPE"
 
     def save_bios_config(self):
