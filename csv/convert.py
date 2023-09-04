0f1d2c3ac312bcf2f4030f08cf6538e372f4f319
Anisse Astier <an.astier@criteo.com>
2023-09-04 18:22:40 +0200

    csv: add export of report_power information
    It puts everything in a different file in the current directory named
    after the results.json file. For example results-monitoring.csv.
    
    Measures are keyed by job_name and job_number that was just added.

--- csv/convert.py
+++ csv/convert.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import sys
+import pathlib
 
 
 def main():
@@ -14,6 +15,12 @@ def main():
     args = parser.parse_args()
     data = json.load(open(args.filename, "r"))
 
+    print_csv_benchmarks_cpu(data)
+
+    create_csv_power(pathlib.Path(args.filename).stem + "-monitoring.csv", data)
+
+
+def print_csv_benchmarks_cpu(data):
     def ok_key(item):
         filtered = {"detail", "cpu_pin", "monitoring"}
         return item not in filtered
@@ -22,27 +29,52 @@ def main():
     csv_keys = list(filter(ok_key, iter(data["bench"].values()).__next__().keys()))
     print(",".join(csv_keys))
 
-    def warn_new_key(item):
-        if item[0] not in csv_keys:
-            print(f"Unknown key {item[0]}", file=sys.stderr)
-
-    def result_key(r):
-        # custom sort order for results using string concatenation
-        return (
-            r.get("engine", "")
-            + r.get("engine_module", "")
-            + r.get("engine_module_parameter", "")
-            + r.get("job_name", "")
-            + f'{r.get("workers", ""):05}'
-            + f'{r.get("job_number", "")}'
-        )
-
     results = sorted(data["bench"].values(), key=result_key)
 
     for result in results:
-        map(warn_new_key, result.items())
+        map(warn_new_key(csv_keys), result.items())
         values = [str(result[key]) for key in csv_keys]
         print(",".join(values))
+
+
+def create_csv_power(out_file_name: str, data):
+    with open(out_file_name, "w") as out:
+        print("job_name,job_number,category,type,unit,event", file=out)
+        results = sorted(data["bench"].values(), key=result_key)
+        for result in results:
+            job_name = result.get("job_name", "")
+            job_number = result.get("job_number", "")
+            monitoring = result.get("monitoring", {})
+            for category in monitoring.keys():
+                for typ in monitoring[category].keys():
+                    measures = monitoring[category][typ]
+                    events = measures.get("events", [])
+                    unit = measures.get("unit")
+                    if not unit:
+                        continue
+                    for event in events:
+                        print(
+                            f"{job_name},{job_number},{category},{typ},{unit},{event}",
+                            file=out,
+                        )
+
+
+def result_key(r):
+    # custom sort order for results using string concatenation
+    return (
+        r.get("engine", "")
+        + r.get("engine_module", "")
+        + r.get("engine_module_parameter", "")
+        + r.get("job_name", "")
+        + f'{r.get("workers", ""):05}'
+        + f'{r.get("job_number", "")}'
+    )
+
+
+def warn_new_key(csv_keys):
+    return lambda item: item[0] not in csv_keys and print(
+        f"Unknown key {item[0]}", file=sys.stderr
+    )
 
 
 if __name__ == "__main__":
