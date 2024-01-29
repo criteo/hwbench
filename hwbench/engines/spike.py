ce78f8879ab754565c7da878d88ec17616420b6e
Erwan Velu <e.velu@criteo.com>
2024-01-29 17:47:53 +0100

    hwbench: Add auto mode to spike engine
    The current spike engines requires to define the low and high values to
    function.
    
    This patch is about adding an automatic mode to avoid interating around
    values to detect a particular behavior.
    
    The algorithm looks like the following :
    - the sum of the fan speed is calibrated to reach a low value over 30
      seconds.
    - server is loaded with stress-ng matrixprod workload.
      the number of logical cpu cores to be used is still defined by the
      usual 'hosting_cpu_cores' and 'stressor_range' parameters.
    - once the fans increased their speed by <x>%, the cpu load is stopped
    - a loop waits the fans to return to their initial fan speed
    - if we have enough time, this loop is restarted
    
    The usual runtime parameter for this case, defines the overal time to
    perform loops of fan spiking. Regarding fan's behavior, the number of
    iteration within the runtime may vary.
    
    At the end of the job, two metrics are added to the results.
    - time_to_high: a list of floats reporting the time needed to reach the
      fan limit before stopping the cpu workload
    - time_to_low: a list of floats reporting the time need to return to the
      initial low fan speed.
    
    By default, the 'auto' mode considers the cpu load must be stopped once
    fans speed up by 5%. This can be overidded by padding a number after the
    auto mode directive like in the following example showing a 'stop if
    fans speed up by 10%' example.
    
    	engine_module_parameter_base=auto:10
    
    This mode works pretty well on chassis that have a single server inside.
    A customized stop value must be adjusted regarding the system : number
    of fans, units and thermal algorithm's sensisivity.
    Note some servers reports their fans in RPM and others in percent. This
    code is computing the percentage on top the sum of these metrics,
    implicating the precision may vary between systems.
    
    This algorithm is a bit less efficient on multi-node chassis because
    each node, even if synchronized, is not getting updates from the BMC at
    the same time :
    - each nodes reads on a time basis the BMC fans' speed
    - BMC on its side compute an average of fan speed to be updated every
      _t_ seconds
    - but all BMCs on the same chassis are not reading, and computing, the
      average _t_ seconds at the same time
    - each servers has a different view of the same chassis : one can see
      the sum of fans at X while the next server see them at X+-10%.
    All all servers cannot be synchronized on the value their read at a
    given time, some node may decide to stop or start the cpu load which
    could be a contradictory state seen by others nodes, influencing them in
    their next decision.
    Some nodes may not see the fans reaching the low values (in a descent)
    because others decided to load them again at the same time.
    Even with this possible issue, it doesn't occurs all time, the job will
    output possible time_to_high and time_to_low values to be used manually
    for fine grained testing.
    
    This code still has some printf enabled to validate the behavior on more
    machines.
    
    Signed-off-by: Erwan Velu <e.velu@criteo.com>

--- hwbench/engines/spike.py
+++ hwbench/engines/spike.py
@@ -2,9 +2,11 @@ import os
 import re
 import subprocess
 import time
+from statistics import mean
 from ..bench.parameters import BenchmarkParameters
 from ..bench.engine import EngineBase, EngineModuleBase
 from ..bench.benchmark import ExternalBench
+from ..bench import monitoring_structs
 from ..utils import helpers as h
 
 
@@ -76,27 +78,41 @@ class Spike(ExternalBench):
         self.high = 0
         self.low = 0
         self.cycle = 0
+        self.auto = False
+        # By default, the auto mode will wait fans to speedup by 5% to stop the load
+        self.auto_fan_ratio = 5
         self.parse_parameters()
 
     def parse_parameters(self):
         runtime = self.parameters.runtime
         for param in self.parameters.get_engine_module_parameter_base().split():
             ressources = re.findall(r"\b(high|low):([0-9]+)\b", param)
-            if not ressources:
-                h.fatal("Missing high or low keywords in configuration file")
-            for ressource, value in ressources:
-                if ressource == "high":
-                    self.high = int(value)
-                else:
-                    self.low = int(value)
+            if ressources:
+                self.auto = False
+                for ressource, value in ressources:
+                    if ressource == "high":
+                        self.high = int(value)
+                    else:
+                        self.low = int(value)
+            if "auto" in param:
+                self.auto = True
+                match = re.search(r"auto:(?P<fan_ratio>[0-9]+)", param)
+                if match:
+                    self.auto_fan_ratio = int(match.group("fan_ratio"))
 
-        # 1 cycle is made of <high> seconds loaded + <low> seconds idle
-        self.cycle = self.low + self.high
-        if self.cycle == 0:
-            h.fatal("No cycle detected, check low and high values")
+                if self.auto_fan_ratio <= 1:
+                    h.fatal("fan_ratio should be greater than 1%")
 
-        if runtime % self.cycle > 0:
-            h.fatal(f"Cycles ({self.cycle}s) are not modulo the runtime ({runtime}s)")
+        if not self.auto:
+            # 1 cycle is made of <high> seconds loaded + <low> seconds idle
+            self.cycle = self.low + self.high
+            if self.cycle == 0:
+                h.fatal("No cycle detected, check low and high values")
+
+            if runtime % self.cycle > 0:
+                h.fatal(
+                    f"Cycles ({self.cycle}s) are not modulo the runtime ({runtime}s)"
+                )
 
     def run_cmd(self) -> list[str]:
         # Let's build the command line to run the tool
@@ -111,41 +127,146 @@ class Spike(ExternalBench):
         # There is no score here. Let's report the runtime.
         return self.parameters.get_result_format()
 
-    def run(self):
-        """Do the spike test."""
+    def get_monotonic_clock(self):
+        """Return the raw clock time, not sensible of ntp adjustments."""
+        return time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
 
-        def get_monotonic_clock():
-            """Return the raw clock time, not sensible of ntp adjustments."""
-            return time.clock_gettime(time.CLOCK_MONOTONIC_RAW)
+    def get_fans_speed(self):
+        raw_fans = (
+            self.parameters.get_monitoring()
+            .vendor.get_bmc()
+            .read_fans()
+            .get(str(monitoring_structs.FanContext.FAN))
+        )
+        return sum([fan.get_values()[-1] for _, fan in raw_fans.items()])
 
-        def since_start():
-            return get_monotonic_clock() - start_time
+    def __spawn_stressor(self, additional_args=[], wait_stressor=False):
+        args = [
+            self.engine_module.engine.get_binary(),
+            "-c",
+            f"{str(self.parameters.get_engine_instances_count())}",
+            "--cpu-method",
+            "matrixprod",
+        ] + additional_args
+        args = self.get_taskset(args)
 
-        # 1 cycle is made of <high> seconds loaded + <low> seconds idle
-        start_time = get_monotonic_clock()
-
-        super().pre_run()
-
-        while True:
-            # Let's load the system from <high> seconds on the listed cores
+        if wait_stressor:
             english_env = os.environ.copy()
             english_env["LC_ALL"] = "C"
-            args = [
-                self.engine_module.engine.get_binary(),
-                "-c",
-                f"{str(self.parameters.get_engine_instances_count())}",
-                "--cpu-method",
-                "matrixprod",
-                "-t",
-                f"{self.high}",
-            ]
-            args = self.get_taskset(args)
-            subprocess.run(
+            return subprocess.run(
                 args,
                 capture_output=True,
                 env=english_env,
                 stdin=subprocess.DEVNULL,
             )
+        else:
+            return subprocess.Popen(
+                args,
+                stdout=subprocess.PIPE,
+                stdin=subprocess.DEVNULL,
+            )
+
+    def auto_spike(self):
+        """Perform automatic spiking"""
+
+        def since_start():
+            return self.get_monotonic_clock() - start_time
+
+        # 1 cycle is made of <high> seconds loaded + <low> seconds idle
+        start_time = self.get_monotonic_clock()
+        super().pre_run()
+
+        # Calibrating low_fan_speed
+        # The low fan speed may vary a bit between the first read and the real low.
+        # This code is making a short calibration over 10 seconds to detect the real low value even if the fans aren't perfectly stable.
+        calibration_fans = []
+        auto_cycle = 11
+        calibration_duration = 3 * auto_cycle
+        print(
+            f"[{self.parameters.get_name()}: calibrating fans for {calibration_duration}s to detect low speed"
+        )
+        for _ in range(0, calibration_duration):
+            calibration_fans.append(self.get_fans_speed())
+            time.sleep(1)
+        # We keep an average value we saw during that period
+        initial_low_fans_speed = mean(calibration_fans)
+
+        fans_speed = initial_low_fans_speed
+        fans_speed_high = initial_low_fans_speed * (100 + self.auto_fan_ratio) / 100
+        wait_high_fans_speed = True
+        time_to_reach_high = []
+        time_to_reach_low = []
+        stressor = None
+        cycle_start = self.get_monotonic_clock()
+        loops = 0
+        while True:
+            loops += 1
+            current_fan_ratio = (fans_speed / initial_low_fans_speed) * 100
+            if loops % 1 == 0:
+                print(
+                    f"initial:{initial_low_fans_speed} current:{fans_speed} percent:{current_fan_ratio:.2f} high_ratio:{self.auto_fan_ratio} high_fan:{fans_speed_high}"
+                )
+            if wait_high_fans_speed:
+                if not stressor:
+                    print("High: Spawing stressor")
+                    stressor = self.__spawn_stressor()
+                elif fans_speed >= fans_speed_high:
+                    # Fans reach the expected speed, let's stop the stress
+                    time_to_reach_high.append(self.get_monotonic_clock() - cycle_start)
+                    wait_high_fans_speed = False
+                    stressor.kill()
+                    stressor = None
+                    print(
+                        f"High: reached {current_fan_ratio:.2f}% with fans={fans_speed}"
+                    )
+                    # Let's reset the cycle start for the low_fan_speed
+                    cycle_start = self.get_monotonic_clock()
+            else:
+                # We are in a descent phase
+                if fans_speed < (initial_low_fans_speed * 1.01):
+                    # We reached the initial fan speed ~1%, we can prepare the next load cycle
+                    time_to_reach_low.append(self.get_monotonic_clock() - cycle_start)
+                    print(
+                        f"Low: reached {current_fan_ratio:.2f}% with fans={fans_speed}"
+                    )
+                    wait_high_fans_speed = True
+
+            # Time-keeper:
+            # Test should not run longer than expected runtime
+            # If the next cycle puts us out of the bondaries, let's stop here
+            # We let a gentle 2 seconds bonus from the time rounding.
+            if since_start() + auto_cycle > self.parameters.get_runtime() + 2:
+                if stressor:
+                    stressor.kill()
+                # Compute the results and return them
+                return super().post_run(
+                    self.parameters.get_result_format()
+                    | {
+                        "time_to_high": time_to_reach_high,
+                        "time_to_low": time_to_reach_low,
+                    }
+                )
+
+            # Let's wait the next cycle
+            time.sleep(auto_cycle)
+
+            # collect the fans speed for the next iteration
+            fans_speed = self.get_fans_speed()
+
+    def manual_spike(self):
+        """Perform a manual spiking"""
+
+        def since_start():
+            return self.get_monotonic_clock() - start_time
+
+        # 1 cycle is made of <high> seconds loaded + <low> seconds idle
+        start_time = self.get_monotonic_clock()
+
+        super().pre_run()
+
+        while True:
+            # Let's load the system from <high> seconds on the listed cores
+            self.__spawn_stressor(["-t", f"{self.high}"], wait_stressor=True)
 
             # Let's the system unload for <low> seconds
             time.sleep(self.low)
@@ -154,9 +275,22 @@ class Spike(ExternalBench):
             # If the next cycle puts us out of the bondaries, let's stop here
             # We let a gentle 2 seconds bonus from the time rounding.
             if since_start() + self.cycle > self.parameters.get_runtime() + 2:
-                return super().post_run(self.parameters.get_result_format())
+                return super().post_run(
+                    self.parameters.get_result_format()
+                    | {
+                        "time_to_high": self.high,
+                        "time_to_low": self.low,
+                    }
+                )
 
             # If we have enough time, let's do another cycle
+
+    def run(self):
+        """Do the spike test."""
+        if self.cycle:
+            return self.manual_spike()
+        elif self.auto:
+            return self.auto_spike()
 
     @property
     def name(self) -> str:
