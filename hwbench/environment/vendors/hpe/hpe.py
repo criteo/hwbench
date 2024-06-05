9ec4b5730996c38e62a7338903971072d2d3d727
Erwan Velu <e.velu@criteo.com>
2024-06-05 10:03:21 +0200

    hwbench/monitoring: Simplify addition of a new metric
    The current code was a great set of code that got copy/paste over
    various functions and vendors.
    
    This generated lots of hard to read code which is error-prone.
    
    This commit is merging all the metric addition logic in BMC.add_monitoring_value()
    so it can be used across all vendors including the mocked one.
    
    Signed-off-by: Erwan Velu <e.velu@criteo.com>

--- hwbench/environment/vendors/hpe/hpe.py
+++ hwbench/environment/vendors/hpe/hpe.py
@@ -1,6 +1,9 @@
 import pathlib
 import re
+from typing import cast
+
 from ....bench.monitoring_structs import (
+    MonitorMetric,
     Power,
     PowerCategories as PowerCat,
     PowerContext,
@@ -28,8 +31,6 @@ class ILO(BMC):
             if t["ReadingCelsius"] <= 0:
                 continue
             pc = t["PhysicalContext"]
-            if pc not in thermals:
-                thermals[pc] = {}
 
             # Temperature metrics are named like the following :
             # 05-P1 DIMM 5-8
@@ -48,20 +49,24 @@ class ILO(BMC):
                 # 04-P1 DIMM 1-4
                 sd = f"{s}{d}"
 
-                def add(name):
-                    if t["Name"] not in thermals[pc]:
-                        thermals[pc][t["Name"]] = Temperature(name)
-                    thermals[pc][t["Name"]].add(t["ReadingCelsius"])
+                def add(self, name):
+                    super().add_monitoring_value(
+                        cast(dict[str, dict[str, MonitorMetric]], thermals),
+                        pc,
+                        Temperature(name),
+                        t["Name"],
+                        t["ReadingCelsius"],
+                    )
 
                 # We don't consider all sensors for now
                 # This could be updated depending on the needs
                 if s == "CPU":
-                    add(sd)
+                    add(self, sd)
                 elif s == "Inlet":
-                    add(s)
+                    add(self, s)
                 elif d == "DIMM":
                     # P1 DIMM 1-4
-                    add(f"{s} {d} {de}")
+                    add(self, f"{s} {d} {de}")
         return thermals
 
     def get_power(self):
@@ -78,10 +83,12 @@ class ILO(BMC):
             # Let's update it to have a unique name
             name = psu["Name"] + str(psu["Oem"]["Hpe"]["BayNumber"])
             psu_name = "PS" + str(psu["Oem"]["Hpe"]["BayNumber"])
-            if name not in power_supplies[str(PowerContext.BMC)]:
-                power_supplies[str(PowerContext.BMC)][name] = Power(psu_name)
-            power_supplies[str(PowerContext.BMC)][name].add(
-                psu["Oem"]["Hpe"]["AveragePowerOutputWatts"]
+            super().add_monitoring_value(
+                cast(dict[str, dict[str, MonitorMetric]], power_supplies),
+                PowerContext.BMC,
+                Power(psu_name),
+                name,
+                psu["Oem"]["Hpe"]["AveragePowerOutputWatts"],
             )
 
         return power_supplies
@@ -98,26 +105,31 @@ class ILO(BMC):
 
         # But for multi-server chassis, ...
         if "HPE Apollo2000 Gen10+" in oem_chassis["Name"]:
-            if str(PowerContext.BMC) not in power_consumption:
-                power_consumption[str(PowerContext.BMC)] = {
-                    str(PowerCat.SERVER): Power(str(PowerCat.SERVER)),
-                    str(PowerCat.CHASSIS): Power(str(PowerCat.CHASSIS)),
-                    str(PowerCat.SERVERINCHASSIS): Power(str(PowerCat.SERVERINCHASSIS)),
-                }  # type: ignore[no-redef]
-
             # On Apollo2000, the generic PowerConsumedWatts is fact SERVERINCHASSIS
-            power_consumption[str(PowerContext.BMC)][str(PowerCat.SERVERINCHASSIS)].add(
-                self.get_power().get("PowerControl")[0]["PowerConsumedWatts"]
+            super().add_monitoring_value(
+                cast(dict[str, dict[str, MonitorMetric]], power_consumption),
+                PowerContext.BMC,
+                Power(str(PowerCat.SERVERINCHASSIS)),
+                str(PowerCat.SERVERINCHASSIS),
+                self.get_power().get("PowerControl")[0]["PowerConsumedWatts"],
             )
 
             # And extract SERVER from NodePowerWatts
-            power_consumption[str(PowerContext.BMC)][str(PowerCat.SERVER)].add(
-                oem_chassis["Oem"]["Hpe"]["NodePowerWatts"]
+            super().add_monitoring_value(
+                cast(dict[str, dict[str, MonitorMetric]], power_consumption),
+                PowerContext.BMC,
+                Power(str(PowerCat.SERVER)),
+                str(PowerCat.SERVER),
+                oem_chassis["Oem"]["Hpe"]["NodePowerWatts"],
             )
 
             # And CHASSIS from ChassisPowerWatts
-            power_consumption[str(PowerContext.BMC)][str(PowerCat.CHASSIS)].add(
-                oem_chassis["Oem"]["Hpe"]["ChassisPowerWatts"]
+            super().add_monitoring_value(
+                cast(dict[str, dict[str, MonitorMetric]], power_consumption),
+                PowerContext.BMC,
+                Power(str(PowerCat.CHASSIS)),
+                str(PowerCat.CHASSIS),
+                oem_chassis["Oem"]["Hpe"]["ChassisPowerWatts"],
             )
         return power_consumption
 
