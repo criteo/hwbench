fb7a04390b0fb5a0183aee7bbd1811fcf96a952a
Erwan Velu <e.velu@criteo.com>
2025-05-02 15:55:09 +0200

    hwbench: Adding basic NVME configuration dump
    This commit dumps nvme-cli output to collect the configuration status of
    the nvme drives.
    
    For now,
    - there is no parsing of the command, to be implemented later
    - nvme list is saved in the output directory
    
    This commit adds a new dependency (nvme-cli) to run hwbench.
    
    Signed-off-by: Erwan Velu <e.velu@criteo.com>

---
+++ hwbench/environment/nvme.py
@@ -0,0 +1,21 @@
+from hwbench.utils.external import External
+
+
+class Nvme(External):
+    def run_cmd(self) -> list[str]:
+        """Dump verbose decoded nvme-cli info"""
+        return ["nvme", "list", "-v", "-ojson"]
+
+    def parse_cmd(self, stdout: bytes, _stderr: bytes):
+        return None
+
+    def run_cmd_version(self) -> list[str]:
+        return ["nvme", "version"]
+
+    def parse_version(self, stdout: bytes, _stderr: bytes) -> bytes:
+        self.version = stdout.split()[2]
+        return self.version
+
+    @property
+    def name(self) -> str:
+        return "nvme-cli"
