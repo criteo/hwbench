1b71436956d2480be1d5ab151eaad14060b685fb
Arno DUBOIS <a.dubois@criteo.com>
2025-07-16 16:47:50 +0200

    cpu_cores: add wsl to testing
    This should ensure the new way works for weird situations

--- hwbench/environment/test_parse.py
+++ hwbench/environment/test_parse.py
@@ -47,157 +47,49 @@ class TestParseCPU:
             assert test_target.get_vendor() == output["Vendor ID"]
 
     def test_parsing_cpu_cores(self):
-        d = pathlib.Path("./hwbench/tests/parsing/cpu_cores/v2321")
-        print(f"parsing test {d.name}")
-        test_target = cpu_cores.CPU_CORES(path)
+        for d in [
+            pathlib.Path("./hwbench/tests/parsing/cpu_cores/v2321"),
+            pathlib.Path("./hwbench/tests/parsing/cpu_cores/cpustorage"),
+            pathlib.Path("./hwbench/tests/parsing/cpu_cores/wsl"),
+        ]:
+            print(f"parsing test {d.name}")
+            test_target = cpu_cores.CPU_CORES(d)
 
-        ver_stdout = (d / "version-stdout").read_bytes()
-        ver_stderr = (d / "version-stderr").read_bytes()
+            ver_stdout = (d / "version-stdout").read_bytes()
+            ver_stderr = (d / "version-stderr").read_bytes()
 
-        version = test_target.parse_version(ver_stdout, ver_stderr)
-        assert version == (d / "version").read_bytes().strip()
+            version = test_target.parse_version(ver_stdout, ver_stderr)
+            assert version == (d / "version").read_bytes().strip()
 
-        stdout = (d / "stdout").read_bytes()
-        stderr = (d / "stderr").read_bytes()
+            stdout = (d / "stdout").read_bytes()
+            stderr = (d / "stderr").read_bytes()
 
-        test_target.parse_cmd(stdout, stderr)
-        assert test_target.get_physical_cores() == [
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18,
-            19,
-            20,
-            21,
-            22,
-            23,
-            24,
-            25,
-            26,
-            27,
-            28,
-            29,
-            30,
-            31,
-            32,
-            33,
-            34,
-            35,
-            36,
-            37,
-            38,
-            39,
-            40,
-            41,
-            42,
-            43,
-            44,
-            45,
-            46,
-            47,
-            48,
-            49,
-            50,
-            51,
-            52,
-            53,
-            54,
-            55,
-            56,
-            57,
-            58,
-            59,
-            60,
-            61,
-            62,
-            63,
-        ]
-        assert test_target.get_hyperthread_cores() == [
-            64,
-            65,
-            66,
-            67,
-            68,
-            69,
-            70,
-            71,
-            72,
-            73,
-            74,
-            75,
-            76,
-            77,
-            78,
-            79,
-            80,
-            81,
-            82,
-            83,
-            84,
-            85,
-            86,
-            87,
-            88,
-            89,
-            90,
-            91,
-            92,
-            93,
-            94,
-            95,
-            96,
-            97,
-            98,
-            99,
-            100,
-            101,
-            102,
-            103,
-            104,
-            105,
-            106,
-            107,
-            108,
-            109,
-            110,
-            111,
-            112,
-            113,
-            114,
-            115,
-            116,
-            117,
-            118,
-            119,
-            120,
-            121,
-            122,
-            123,
-            124,
-            125,
-            126,
-            127,
-        ]
-        assert test_target.get_logical_cores_count() == 128
-        assert test_target.get_physical_cores_count() == 64
-        assert test_target.get_peer_siblings(0) == [0, 64]
-        assert test_target.get_peer_sibling(0) == 64
-        assert test_target.get_peer_sibling(64) == 0
+            test_target.parse_cmd(stdout, stderr)
+
+            if d.name == "v2321":
+                assert test_target.get_physical_cores() == list(range(64))
+                assert test_target.get_hyperthread_cores() == list(range(64, 128))
+                assert test_target.get_logical_cores_count() == 128
+                assert test_target.get_physical_cores_count() == 64
+                assert test_target.get_peer_siblings(0) == [0, 64]
+                assert test_target.get_peer_sibling(0) == 64
+                assert test_target.get_peer_sibling(64) == 0
+
+            if d.name == "cpustorage":
+                assert test_target.get_physical_cores() == list(range(36))
+                assert test_target.get_hyperthread_cores() == list(range(36, 72))
+                assert test_target.get_logical_cores_count() == 72
+                assert test_target.get_physical_cores_count() == 36
+                assert test_target.get_peer_siblings(0) == [0, 36]
+                assert test_target.get_peer_sibling(0) == 36
+
+            if d.name == "wsl":
+                assert test_target.get_physical_cores() == list(range(6))
+                assert test_target.get_hyperthread_cores() == list(range(1, 12, 2))
+                assert test_target.get_logical_cores_count() == 12
+                assert test_target.get_physical_cores_count() == 6
+                assert test_target.get_peer_siblings(0) == [0, 1]
+                assert test_target.get_peer_sibling(0) == 1
 
     def test_parsing_numa_1_domain(self):
         d = pathlib.Path("./hwbench/tests/parsing/numa/1domain")
